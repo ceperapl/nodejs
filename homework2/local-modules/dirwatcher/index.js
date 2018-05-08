3b144c1ccb2bf74defc2e2d80061bc89c8fee750
Siarhei Pazdniakou <siarhei_pazdniakou1@epam.com>
2018-05-08 22:13:54 +0300

    Fix remove bug

--- homework2/local-modules/dirwatcher/index.js
+++ homework2/local-modules/dirwatcher/index.js
@@ -42,7 +42,6 @@ export default class DirWatcher extends EventEmitter {
           if (!files.find(file => file === filePath)) {
             this.emit('removed', filePath);
             delete this.files[filePath];
-            break;
           }
         }
       })
